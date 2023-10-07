feb45b7dc93e81029b97ef8304107ee19b88e53b
Mushogenshin <mushogenshin@gmail.com>
2023-10-07 17:13:26 +0700

    VS Code task

---
+++ scripts/copy_to_addons.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Copy the FBX2Rigify folder to Blender's addons directory
+cp -R /Users/mushogenshin/projects/FBX2Rigify /Applications/Blender.app/Contents/Resources/3.6/scripts/addons/
